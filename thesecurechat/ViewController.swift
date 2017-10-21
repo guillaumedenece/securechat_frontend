6753ff0867e790e497340e87086c8bcd97f7d788
Nicolas Chevrier <nicoescrime25@hotmail.fr>
2017-10-20 19:18:36 -0700

    Initial Commit

---
+++ thesecurechat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  thesecurechat
+//
+//  Created by Nicolas Chevrier on 20/10/2017.
+//  Copyright © 2017 Nicolas Chevrier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
