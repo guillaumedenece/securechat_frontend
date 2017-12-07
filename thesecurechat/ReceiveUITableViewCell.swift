31022ef7a237a9ec4a55ee1bb9583ada177645c0
Nicolas Chevrier <nicolas.chevrier@outlook.fr>
2017-12-06 19:13:40 -0800

    restore files

---
+++ thesecurechat/ReceiveUITableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RecceiveUITableViewCell.swift
+//  thesecurechat
+//
+//  Created by Nicolas Chevrier on 06/12/2017.
+//  Copyright © 2017 Nicolas Chevrier. All rights reserved.
+//
+
+import UIKit
+
+class ReceiveUITableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
